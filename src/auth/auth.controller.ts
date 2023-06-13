9db350902250bc1343aa566bd87079001e6b8c89
Azamat R <azamat@rasulov.dev>
2023-06-13 22:48:56 +0500

    feat: sign in, get jwt

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,20 +1,27 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common'
+import { Body, Controller, Get, HttpCode, Post, Query } from '@nestjs/common'
 import { AuthService } from './auth.service'
 import { AccountValidationDto } from './dto/account-validation.dto'
+import { SignInDto } from './dto/sign-in.dto'
 import { SignUpDto } from './dto/sign-up.dto'
+import { SignInResponse } from './types/sign-in.response'
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly _service: AuthService) {}
 
   @Post('sign-up')
-  public signUp(@Body() dto: SignUpDto): unknown {
+  public async signUp(@Body() dto: SignUpDto): Promise<number> {
     return this._service.signUp(dto)
   }
 
   @HttpCode(200)
   @Post('sign-up/validate')
-  public validate(@Body() dto: AccountValidationDto): unknown {
+  public async validate(@Body() dto: AccountValidationDto): Promise<void> {
     return this._service.validateAccountExistence(dto)
+  }
+
+  @Get('sign-in')
+  public async signIn(@Query() dto: SignInDto): Promise<SignInResponse> {
+    return this._service.signIn(dto)
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,26 @@
-import { ConflictException, Injectable } from '@nestjs/common'
+import {
+  ConflictException,
+  ForbiddenException,
+  Injectable
+} from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { JwtService } from '@nestjs/jwt'
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library'
 import * as argon from 'argon2'
 import to from 'await-to-js'
 import { DbService } from 'src/db/db.service'
 import { AccountValidationDto } from './dto/account-validation.dto'
+import { SignInDto } from './dto/sign-in.dto'
 import { SignUpDto } from './dto/sign-up.dto'
+import { SignInResponse } from './types/sign-in.response'
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly _db: DbService) {}
+  constructor(
+    private readonly _db: DbService,
+    private readonly _jwt: JwtService,
+    private readonly _config: ConfigService
+  ) {}
 
   public async signUp({ email, password }: SignUpDto): Promise<number> {
     const [error, user] = await to(
@@ -38,6 +50,28 @@ export class AuthService {
 
     throw new ConflictException(
       "There's an account registered with this email already"
+    )
+  }
+
+  public async signIn({ email, password }: SignInDto): Promise<SignInResponse> {
+    const user = await this._db.user.findUnique({ where: { email } })
+
+    if (!user || !(await argon.verify(user.password, password))) {
+      throw new ForbiddenException('Incorrect credentials')
+    }
+
+    return {
+      accessToken: await this._generateAccessToken(user.id, user.email)
+    }
+  }
+
+  private async _generateAccessToken(
+    userId: number,
+    email: string
+  ): Promise<string> {
+    return this._jwt.signAsync(
+      { sub: userId, email },
+      { expiresIn: '10m', secret: this._config.get('JWT_SECRET') }
     )
   }
 }
